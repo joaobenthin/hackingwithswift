7ab2968e7f72927b44a2b0c24529e6c5de9fec43
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2024-02-05 21:41:37 -0300

    How to reuse code with functions

---
+++ Day7-01.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Cocoa
+
+func showWelcome() {
+    print("Welcome to my app!")
+    print("By default This prints out a conversion")
+    print("chart from centimeters to inches, but you")
+    print("can also set a custom range if you want.")
+}
+
+showWelcome()
+
+
+let number = 139
+
+if number.isMultiple(of: 2) {
+    print("Even")
+} else {
+    print("Odd")
+}
+
+
+let roll = Int.random(in: 1...20)
+
+func printTimesTable(number: Int, end: Int) {
+    for i in 1...end {
+        print("\(i) * \(number) is \(i * number)")
+    }
+}
+
+printTimesTable(number: 5, end: 20)
